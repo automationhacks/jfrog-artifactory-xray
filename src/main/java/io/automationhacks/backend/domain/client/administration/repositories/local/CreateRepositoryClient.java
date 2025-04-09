ce315fbfa363eb1dbc5bf029934d6b8009630cdd
automationhacks <automationhacks@gmail.com>
2025-04-09 22:07:13 +0530

    - Updated Create repository client to use request specification to form the base of sharing auth information - Updated test to set the repo name in path param

--- src/main/java/io/automationhacks/backend/domain/client/administration/repositories/local/CreateRepositoryClient.java
+++ src/main/java/io/automationhacks/backend/domain/client/administration/repositories/local/CreateRepositoryClient.java
@@ -3,21 +3,26 @@ package io.automationhacks.backend.domain.client.administration.repositories.loc
 import static io.restassured.RestAssured.given;
 
 import io.automationhacks.backend.core.api.APIResponse;
-import io.automationhacks.backend.core.api.http.HttpMethod;
 import io.automationhacks.backend.core.constants.Auth;
 import io.automationhacks.backend.domain.constants.Endpoints;
+import io.restassured.specification.RequestSpecification;
 
 public class CreateRepositoryClient {
     public APIResponse createRepository(String repoKey, String body) {
         String url = Endpoints.CREATE_REPOSITORY_URL.formatted(Endpoints.BASE_URL);
 
         var auth = new Auth().getCredentials();
-        var response =
+        RequestSpecification requestSpecification =
                 given().auth()
                         .basic(auth.username(), auth.password())
+                        .header("Content-Type", "application/json");
+
+        var response =
+                given().spec(requestSpecification)
                         .pathParam("repoKey", repoKey)
+                        .body(body)
                         .when()
-                        .request(HttpMethod.PUT.toString(), url, body);
+                        .put(url);
 
         return APIResponse.builder()
                 .statusCode(response.getStatusCode())
